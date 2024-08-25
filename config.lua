bab3c82f32855c1330fbfe2e941e231d00d3c96a
Mitch <94312901+mitch0185@users.noreply.github.com>
2024-08-25 03:04:13 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,30 @@
+Config = {}
+
+Config.NotifyMethod = 'esx_notify'
+
+Config.Prices = {
+    ['adder'] = 1000,
+    ['banshee'] = 800,
+    ['sultan'] = 500
+}
+
+Config.Vehicles = {
+    'adder',
+    'banshee',
+    'sultan'
+}
+
+Config.VehicleSpawnLocation = {
+    x = -42.4,
+    y = -1097.0,
+    z = 26.4,
+    heading = 70.0
+}
+
+Config.NPCPosition = {
+    x = -45.0,
+    y = -1096.0,
+    z = 26.4,
+    heading = 160.0,
+    model = 'a_m_m_business_01'
+}
