7855805357d85504eb37a531305ab6a9a671d926
Mitch <94312901+mitch0185@users.noreply.github.com>
2024-08-25 03:06:50 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Mitch'
+description 'Simple car rent script'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua'
+}
+
+server_scripts {
+    'server.lua'
+}
