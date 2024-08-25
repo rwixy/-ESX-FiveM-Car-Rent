cc197809d6f5e2a7a2e17075716b5f6bede29459
Mitch <94312901+mitch0185@users.noreply.github.com>
2024-08-25 03:05:40 +0200

    Create server.lua

---
+++ server.lua
@@ -0,0 +1,16 @@
+ESX = nil
+
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterNetEvent('vehicle_rental:rentVehicle')
+AddEventHandler('vehicle_rental:rentVehicle', function(vehicleModel)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local price = Config.Prices[vehicleModel]
+
+    if xPlayer.getMoney() >= price then
+        xPlayer.removeMoney(price)
+        TriggerClientEvent('vehicle_rental:spawnVehicle', source, vehicleModel)
+    else
+        TriggerClientEvent('vehicle_rental:notify', source, 'Nicht genug Geld!')
+    end
+end)
